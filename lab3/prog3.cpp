27af66e2e95503c4be1bf76bb1f11581b1f539a8
Kelyn Thomas <5085259@nwpolytech.ca>
2024-09-26 16:14:41 -0600

    Included program 3.

---
+++ lab3/prog3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	typedef int* VEC;
+	typedef VEC MATRIX;
+
+	int a = 5;
+	VEC v = &a;
+	MATRIX m = v;
+
+	cout << "The value of a from v is: " << *v;
+	cout << "\nThe value of a from m is: " << *m;
+
+	return 0;
+}
