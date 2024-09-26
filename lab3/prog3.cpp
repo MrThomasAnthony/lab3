ef6cf82f9653fd2d87a6c72792c096c5ccafc494
Kelyn Thomas <5085259@nwpolytech.ca>
2024-09-26 16:42:52 -0600

    Updated prog3: Pointing to a pointer! (**m).

--- lab3/prog3.cpp
+++ lab3/prog3.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main() {
 	
 	typedef int* VEC;
-	typedef VEC MATRIX;
+	typedef VEC* MATRIX;
 
 	int a = 5;
 	VEC v = &a;
-	MATRIX m = v;
+	MATRIX m = &v;
 
 	cout << "The value of a from v is: " << *v;
-	cout << "\nThe value of a from m is: " << *m;
+	cout << "\nThe value of a from m is: " << **m;
 
 	return 0;
 }
